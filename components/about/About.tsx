ecfd983e01f249f0a49b9d8ea69eefd0fb147b8f
mikkio-j <mikkiodev@gmail.com>
2021-10-09 12:31:18 +0200

    add about me boxes

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,12 +1,46 @@
 import styles from "./About.module.scss";
+import { IoBookSharp } from "react-icons/io5";
+import { GiLightBulb, GiHumanPyramid } from "react-icons/gi";
 
 const About = () => {
   return (
     <section id="about" className={styles.aboutWrapper}>
-      <p className={styles.aboutmeText}>O mnie</p>
-      <h2 className={styles.aboutmeDescription}>
+      <p className={styles.aboutHeader}>O mnie</p>
+      <h2 className={styles.aboutDescription}>
         moim celem jest Twoja satysfakcja
       </h2>
+      <div className={styles.aboutInnerWrapper}>
+        <div className={styles.aboutIconBoxWrapper}>
+          <div className={styles.aboutIconBoxPrimary}>
+            <IoBookSharp size="50px" />
+          </div>
+          <h2 className={styles.aboutIconBoxTitle}>Wykształcenie</h2>
+          <p className={styles.aboutIconBoxDescription}>
+            Stopień magistra Uniwersytetu Warszawskiego w dziedzinach
+            translatoryki i pedagogiki nauczania języków obcych.
+          </p>
+        </div>
+        <div className={styles.aboutIconBoxWrapper}>
+          <div className={styles.aboutIconBoxSecondary}>
+            <GiLightBulb size="50px" />
+          </div>
+          <h2 className={styles.aboutIconBoxTitle}>Doświadczenie</h2>
+          <p className={styles.aboutIconBoxDescription}>
+            Wiele lat doświadczenia w sektorze edukacji oraz stażu w roli
+            tłumacza pisemnego i lokalizatora gier.
+          </p>
+        </div>
+        <div className={styles.aboutIconBoxWrapper}>
+          <div className={styles.aboutIconBoxTertiary}>
+            <GiHumanPyramid size="50px" />
+          </div>
+          <h2 className={styles.aboutIconBoxTitle}>Podejście</h2>
+          <p className={styles.aboutIconBoxDescription}>
+            Bez pośredników - przetłumaczę Twoje treści bezpośrednio dla Ciebie
+            i zadbam, aby wiernie reprezentowały Twój zamysł.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
