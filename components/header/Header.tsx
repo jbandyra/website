3a560004f373da770539e0009cfc7f5e1ee31913
Mikolaj J <mikkiodev@gmail.com>
2021-12-02 19:04:47 +0100

    add prettier, change flags svg, fix toggle hover

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,158 +1,168 @@
-import { useState, useEffect } from "react";
-import { useTranslation } from "next-i18next";
-import Image from "next/image";
-import Link from "next/link";
-import Toggle from "@components/toggle/Toggle";
+import { useState, useEffect } from 'react'
+import { useTranslation } from 'next-i18next'
+import Image from 'next/image'
+import Link from 'next/link'
+import Toggle from '@components/toggle/Toggle'
 
-import Logo from "@assets/logo.png";
-import LogoDark from "@assets/logo-dark.png";
-import styles from "./Header.module.scss";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { VscClose } from "react-icons/vsc";
+import Logo from '@assets/logo.png'
+import LogoDark from '@assets/logo-dark.png'
+import styles from './Header.module.scss'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { VscClose } from 'react-icons/vsc'
 
-import { useLocale } from "@hooks/useLocaleSwitch";
+import { useLocale } from '@hooks/useLocaleSwitch'
 
 const Header = ({ isDarkModeOn, handleDarkModeToggle, secondary = false }) => {
-  const { switchLocale, localeEmoji } = useLocale();
+    const { switchLocale, localeEmoji } = useLocale()
 
-  const { t } = useTranslation("common");
+    const { t } = useTranslation('common')
 
-  const [offsetY, setOffsetY] = useState(0);
-  const [isMobileNavOn, setMobileNav] = useState(false);
+    const [offsetY, setOffsetY] = useState(0)
+    const [isMobileNavOn, setMobileNav] = useState(false)
 
-  const menuItems = [
-    { label: t("home"), path: "/#home" },
-    { label: t("about"), path: "/#about" },
-    { label: t("contact"), path: "/#contact" },
-    {
-      label: localeEmoji,
-      onClick: switchLocale,
-      styles: styles.navDarkToggle,
-      mobileNavDisabled: true,
-    },
-  ];
-  useEffect(() => {
-    const handleScroll = () => setOffsetY(window.pageYOffset);
-    window.addEventListener("scroll", handleScroll);
+    const menuItems = [
+        { label: t('home'), path: '/#home' },
+        { label: t('about'), path: '/#about' },
+        { label: t('contact'), path: '/#contact' },
+        {
+            label: localeEmoji,
+            onClick: switchLocale,
+            styles: styles.navDarkToggle,
+            mobileNavDisabled: true,
+        },
+    ]
+    useEffect(() => {
+        const handleScroll = () => setOffsetY(window.pageYOffset)
+        window.addEventListener('scroll', handleScroll)
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  useEffect(() => {
-    if (isMobileNavOn) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+    useEffect(() => {
+        if (isMobileNavOn) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'auto'
+        }
+    }, [isMobileNavOn])
+
+    const getNavStyles = () => {
+        switch (true) {
+            case offsetY > 200:
+                return styles.headerWrapperStickyFifth
+            case offsetY > 125:
+                return styles.headerWrapperStickyFourth
+            case offsetY > 75:
+                return styles.headerWrapperStickyThird
+            case offsetY > 50:
+                return styles.headerWrapperStickySecond
+            case offsetY > 1:
+                return styles.headerWrapperStickyFirst
+            default:
+                return styles.headerWrapper
+        }
     }
-  }, [isMobileNavOn]);
 
-  const getNavStyles = () => {
-    switch (true) {
-      case offsetY > 200:
-        return styles.headerWrapperStickyFifth;
-      case offsetY > 125:
-        return styles.headerWrapperStickyFourth;
-      case offsetY > 75:
-        return styles.headerWrapperStickyThird;
-      case offsetY > 50:
-        return styles.headerWrapperStickySecond;
-      case offsetY > 1:
-        return styles.headerWrapperStickyFirst;
-      default:
-        return styles.headerWrapper;
-    }
-  };
-
-  return (
-    <>
-      <nav className={getNavStyles()}>
-        <div className={styles.headerInnerWrapper}>
-          <Link href="/">
-            <a>
-              <Image
-                src={
-                  (offsetY > 75 && !isDarkModeOn) ||
-                  (secondary && !isDarkModeOn)
-                    ? LogoDark
-                    : Logo
-                }
-                width="60px"
-                height="60px"
-                alt="Logo"
-              />
-            </a>
-          </Link>
-          <ul
-            className={
-              offsetY > 75 || secondary
-                ? styles.headerListSticky
-                : styles.headerList
-            }
-          >
-            {menuItems.map((item, idx) => (
-              <li
-                key={idx}
-                onClick={() => (item.onClick ? item.onClick() : null)}
-                className={item.styles ?? null}
-              >
-                {item.path ? (
-                  <Link href={item.path}>{item.label}</Link>
-                ) : (
-                  item.label
-                )}
-              </li>
-            ))}
-            <li>
-              <Toggle
-                handleOnClick={handleDarkModeToggle}
-                active={!isDarkModeOn}
-              />
-            </li>
-          </ul>
-          <div className={styles.mobileMenu}>
-            <Toggle
-              className="mobile"
-              handleOnClick={handleDarkModeToggle}
-              active={!isDarkModeOn}
-            />
-            <div
-              className={
-                offsetY > 75 || secondary
-                  ? styles.headerHamburgerDark
-                  : styles.headerHamburger
-              }
-            >
-              <GiHamburgerMenu size="32px" onClick={() => setMobileNav(true)} />
+    return (
+        <>
+            <nav className={getNavStyles()}>
+                <div className={styles.headerInnerWrapper}>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src={
+                                    (offsetY > 75 && !isDarkModeOn) ||
+                                    (secondary && !isDarkModeOn)
+                                        ? LogoDark
+                                        : Logo
+                                }
+                                width="60px"
+                                height="60px"
+                                alt="Logo"
+                            />
+                        </a>
+                    </Link>
+                    <ul
+                        className={
+                            offsetY > 75 || secondary
+                                ? styles.headerListSticky
+                                : styles.headerList
+                        }
+                    >
+                        {menuItems.map((item, idx) => (
+                            <li
+                                key={idx}
+                                onClick={() =>
+                                    item.onClick ? item.onClick() : null
+                                }
+                                className={item.styles ?? null}
+                            >
+                                {item.path ? (
+                                    <Link href={item.path}>{item.label}</Link>
+                                ) : (
+                                    item.label
+                                )}
+                            </li>
+                        ))}
+                        <Toggle
+                            handleOnClick={handleDarkModeToggle}
+                            active={!isDarkModeOn}
+                        />
+                    </ul>
+                    <div className={styles.mobileMenu}>
+                        <Toggle
+                            className="mobile"
+                            handleOnClick={handleDarkModeToggle}
+                            active={!isDarkModeOn}
+                        />
+                        <div
+                            className={
+                                offsetY > 75 || secondary
+                                    ? styles.headerHamburgerDark
+                                    : styles.headerHamburger
+                            }
+                        >
+                            <GiHamburgerMenu
+                                size="32px"
+                                onClick={() => setMobileNav(true)}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div className={isMobileNavOn ? styles.menuOpen : styles.menu}>
+                <VscClose
+                    className={
+                        offsetY > 75 ? styles.closeIcon : styles.closeIconDark
+                    }
+                    size="44px"
+                    onClick={() => setMobileNav(false)}
+                />
+                <ul>
+                    {menuItems.map((item, idx) =>
+                        !item.mobileNavDisabled ? (
+                            <li
+                                key={idx}
+                                onClick={() => setMobileNav(false)}
+                                className={
+                                    item.styles ?? offsetY > 290
+                                        ? styles.darkListItem
+                                        : ''
+                                }
+                            >
+                                <Link href={item.path}>{item.label}</Link>
+                            </li>
+                        ) : null
+                    )}
+                </ul>
+                <div
+                    className={styles.mobileLanguageToggle}
+                    onClick={switchLocale}
+                >
+                    {localeEmoji}
+                </div>
             </div>
-          </div>
-        </div>
-      </nav>
-      <div className={isMobileNavOn ? styles.menuOpen : styles.menu}>
-        <VscClose
-          className={offsetY > 75 ? styles.closeIcon : styles.closeIconDark}
-          size="44px"
-          onClick={() => setMobileNav(false)}
-        />
-        <ul>
-          {menuItems.map((item, idx) =>
-            !item.mobileNavDisabled ? (
-              <li
-                key={idx}
-                onClick={() => setMobileNav(false)}
-                className={
-                  item.styles ?? offsetY > 290 ? styles.darkListItem : ""
-                }
-              >
-                <Link href={item.path}>{item.label}</Link>
-              </li>
-            ) : null
-          )}
-        </ul>
-        <div className={styles.mobileLanguageToggle} onClick={switchLocale}>
-          {localeEmoji}
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
 
-export default Header;
+export default Header
--- hooks/useLocaleSwitch.tsx
+++ hooks/useLocaleSwitch.tsx
@@ -1,28 +1,34 @@
-import { createContext, useContext } from "react";
-import { useRouter } from "next/router";
-import ReactCountryFlag from "react-country-flag";
+import { createContext, useContext } from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
-const LocaleContext = createContext(null);
+const LocaleContext = createContext(null)
 
-export const useLocale = () => useContext(LocaleContext);
+export const useLocale = () => useContext(LocaleContext)
 
 export const LocaleProvider = ({ children }) => {
-  const router = useRouter();
-  const { locale, pathname, asPath, query } = router;
-  const toPL = () =>
-    router.push({ pathname, query }, asPath, { locale: "pl", scroll: false });
-  const toEN = () =>
-    router.push({ pathname, query }, asPath, { locale: "en", scroll: false });
-  const switchLocale = () => (locale === "pl" ? toEN() : toPL());
-  const localeEmoji =
-    locale === "pl" ? (
-      <ReactCountryFlag countryCode="GB" svg />
-    ) : (
-      <ReactCountryFlag countryCode="PL" svg />
-    );
-  return (
-    <LocaleContext.Provider value={{ locale, switchLocale, localeEmoji }}>
-      {children}
-    </LocaleContext.Provider>
-  );
-};
+    const router = useRouter()
+    const { locale, pathname, asPath, query } = router
+    const toPL = () =>
+        router.push({ pathname, query }, asPath, {
+            locale: 'pl',
+            scroll: false,
+        })
+    const toEN = () =>
+        router.push({ pathname, query }, asPath, {
+            locale: 'en',
+            scroll: false,
+        })
+    const switchLocale = () => (locale === 'pl' ? toEN() : toPL())
+    const localeEmoji =
+        locale === 'pl' ? (
+            <Image src="/british_flag.svg" width="40px" height="40px" />
+        ) : (
+            <Image src="/polish_flag.svg" width="40px" height="40px" />
+        )
+    return (
+        <LocaleContext.Provider value={{ locale, switchLocale, localeEmoji }}>
+            {children}
+        </LocaleContext.Provider>
+    )
+}
